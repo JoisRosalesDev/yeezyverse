ececb4c66be1e43c38a158ffeaa9a617a9bd3a8d
JoisRosalesDev <joisrosafer@gmail.com>
2025-02-02 13:25:33 -0300

    modificación en los componentes

---
+++ pages/awards.js
@@ -0,0 +1,8 @@
+// pages/awards.js
+export default function Awards() {
+    return (
+      <div>
+        <h1>Awards Page</h1>
+      </div>
+    );
+  }
---
+++ pages/discography.js
@@ -0,0 +1,10 @@
+// pages/discography.js
+const Discography = () => {
+    return (
+      <div>
+        <h1>Discography Page</h1>
+      </div>
+    );
+  };
+  
+  export default Discography; // Correcto
