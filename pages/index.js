2e1d53ec9d0229972e141d31787d276fa49042ab
JoisRosalesDev <joisrosafer@gmail.com>
2025-02-05 19:55:04 -0300

    mejoras en el about

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
 
 const TypingText = ({ text, speed = 30 }) => {
     const [displayedText, setDisplayedText] = useState("");
@@ -21,6 +22,79 @@ const TypingText = ({ text, speed = 30 }) => {
 
     return <span>{displayedText}</span>;
 };
+
+const testimonials = [
+    {
+        quote:
+            "Kanye Omari West nació el 8 de junio de 1977 en Atlanta, Georgia, y se trasladó a Chicago, Illinois, con su madre cuando tenía tres años.",
+        name: "Nacimiento y primeros años",
+        designation: "Fuente: Britannica",
+        src: "/young-kanye.webp",
+    },
+    {
+        quote:
+            "A finales de los años 90, Kanye comenzó a trabajar como productor musical, produciendo canciones para artistas como Jay-Z, Alicia Keys y Janet Jackson.",
+        name: "Inicios en la producción musical",
+        designation: "Fuente: Britannica",
+        src: "/studio-kanye.webp",
+    },
+    {
+        quote:
+            "En 2002, Kanye sufrió un accidente automovilístico que casi le cuesta la vida y le dejó fracturada la mandíbula. Este incidente inspiró su primer sencillo en solitario, 'Through the Wire'.",
+        name: "Accidente automovilístico",
+        designation: "Fuente: Britannica",
+        src: "/crashed-kanye.webp",
+    },
+    {
+        quote:
+            "En 2004, lanzó su álbum debut, 'The College Dropout', que recibió elogios de la crítica y el público, marcando su entrada como solista en la industria musical.",
+        name: "Debut como solista",
+        designation: "Fuente: Britannica",
+        src: "/tcd-cover.jpg",
+    },
+    {
+        quote:
+            "Su segundo álbum, 'Late Registration' (2005), y su tercer álbum, 'Graduation' (2007), consolidaron su éxito, con éxitos como 'Gold Digger' y 'Stronger'.",
+        name: "Éxito continuo",
+        designation: "Fuente: Britannica",
+        src: "/late-cover.webp"
+    },
+    {
+        quote:
+            "En 2009, Kanye interrumpió el discurso de aceptación de Taylor Swift en los MTV Video Music Awards, lo que generó una gran controversia.",
+        name: "Incidente en los MTV Video Music Awards",
+        designation: "Fuente: Britannica",
+        src: "/mtv-kanye.webp"
+    },
+    {
+        quote:
+            "Kanye comenzó a trabajar en la moda y lanzó su propia línea de ropa, colaborando con marcas como Nike para diseñar el famoso zapato 'Air Yeezy'.",
+        name: "Exploración de la moda",
+        designation: "Fuente: Britannica",
+        src: "/air-yeezy.webp"
+    },
+    {
+        quote:
+            "En 2007, su madre, Donda West, falleció tras una cirugía estética, lo que afectó profundamente a Kanye.",
+        name: "Problemas personales",
+        designation: "Fuente: Britannica",
+        src: "/donda-west.webp"
+    },
+    {
+        quote:
+            "En 2010, lanzó 'My Beautiful Dark Twisted Fantasy', considerado uno de sus mejores trabajos y aclamado por la crítica.",
+        name: "Álbum aclamado",
+        designation: "Fuente: Britannica",
+        src: "/mbdtf-cover.jpg"
+    },
+    {
+        quote:
+            "En 2020, Kanye anunció su candidatura a la presidencia de los Estados Unidos, aunque finalmente no se presentó oficialmente.",
+        name: "Carrera política",
+        designation: "Fuente: Britannica",
+        src: "/president-kanye.webp"
+    },
+];
 
 export default function Home() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -90,50 +164,14 @@ export default function Home() {
 
             </div>
 
-            {/* About Section */}
-            <section id="about" className="py-20 px-6 text-white bg-primary">
-                {/* Título */}
-                <h3 className="text-4xl md:text-5xl font-bold font-sans text-center mb-16">
+            <section id="about" className="py-20 px-4 text-white bg-primary">
+                {/* Título de la sección */}
+                <h3 className="text-4xl md:text-5xl font-bold text-center mb-16">
                     Acerca de Kanye West
                 </h3>
 
-                {/* Contenedor para la imagen y las cards de información */}
-                <div className="flex flex-col md:flex-row items-center gap-8">
-                    {/* Imagen con sombra de gradiente */}
-                    <div className="order-1 w-full md:w-auto relative rounded-2xl overflow-hidden shadow-[0px_0px_10px_5px_#76606a]">
-                        <Image
-                            src="/kanye-grammy.webp"
-                            alt="Kanye West"
-                            width={300}
-                            height={300}
-                            className="w-full md:w-auto h-auto rounded-2xl mx-auto bg-opacity-80 backdrop-blur"
-                        />
-                    </div>
-
-                    {/* Cards con información */}
-                    <div className="order-2 w-full flex flex-col space-y-6">
-                        <div className="bg-gray-500 bg-opacity-10 backdrop-blur p-6 rounded-2xl border border-zinc-800">
-                            <p className="text-left">
-                                Kanye West (nacido el 8 de junio de 1977 en Atlanta, Georgia) es un rapero, productor musical, diseñador de moda y empresario estadounidense. Reconocido por su innovación en la industria musical, ha sido una figura clave en la evolución del hip-hop y la cultura popular contemporánea.
-                            </p>
-                        </div>
-                        <div className="bg-gray-500 bg-opacity-10 backdrop-blur p-6 rounded-2xl border border-zinc-800">
-                            <p className="text-left">
-                                West inició su carrera como productor en la escena de Chicago, ganando reconocimiento a principios de los 2000 por su trabajo con artistas como Jay-Z. En 2004, debutó como solista con <em>The College Dropout</em>, un álbum aclamado por la crítica que lo estableció como un artista versátil y creativo.
-                            </p>
-                        </div>
-                        <div className="bg-gray-500 bg-opacity-10 backdrop-blur p-6 rounded-2xl border border-zinc-800">
-                            <p className="text-left">
-                                Más allá de la música, ha incursionado en la moda con su línea Yeezy, en colaboración con Adidas, revolucionando el mercado de la ropa y el calzado deportivo. También ha generado polémica por sus declaraciones públicas y su incursión en la política, incluyendo su candidatura presidencial en 2020.
-                            </p>
-                        </div>
-                        <div className="bg-gray-500 bg-opacity-10 backdrop-blur p-6 rounded-2xl border border-zinc-800">
-                            <p className="text-left">
-                                Con múltiples premios Grammy y un impacto cultural significativo, Kanye West sigue siendo una de las figuras más influyentes del siglo XXI, dejando una huella tanto en la música como en la moda y el entretenimiento.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                {/* Carrusel de testimonios animados */}
+                <AnimatedTestimonials testimonials={testimonials} />
             </section>
 
             {/* Discografía */}
@@ -255,7 +293,7 @@ export default function Home() {
             </section>
 
             {/* Footer */}
-            <footer className="p-10 text-center bg-blue-700">
+            <footer className="py-20 px-4 text-center bg-secondary">
                 <div>
                     <p className="">Desarrollado por Jois Rosales</p>
                     <p className="text-sm">© 2025 YeezyVerse. Todos los derechos reservados.</p>
